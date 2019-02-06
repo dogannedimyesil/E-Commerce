3f1b5a5ccb95255a453c7b0dfe4daccc95b20d2f
Sira3 <dogannedimyesil@gmail.com>
2019-02-06 12:43:09 +0300

    d4

--- E-Commerce/Models/Category.cs
+++ E-Commerce/Models/Category.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,13 @@ namespace E_Commerce.Models
 {
     public class Category
     {
-        [Key]
+        [Key, ForeignKey("ParentCategory")]
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Product> Products { get; set; }
+        
+        public List<Category> SubCategories { get; set; }
+        public Category ParentCategory { get; set; }
     }
+
 }
