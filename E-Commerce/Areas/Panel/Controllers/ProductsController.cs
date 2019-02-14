b33aafbcdb5b3d3d6d185cc6d1fb0b1c681b80de
busra <ASUS PC@ASUS>
2019-02-14 11:22:30 +0300

    kategori, ürün, ürün detay

--- E-Commerce/Areas/Panel/Controllers/ProductsController.cs
+++ E-Commerce/Areas/Panel/Controllers/ProductsController.cs
@@ -25,17 +25,18 @@ namespace E_Commerce.Areas.Panel.Controllers
         [HttpPost]
         public ActionResult Create(Product newProduct, HttpPostedFileBase[] productImage, int CategoryIds,  Size sizes)
         {
-           
             newProduct.CategoryId = CategoryIds;
-         
             newProduct.Size = sizes;
             newProduct.ProductImages = new List<ProductImage>();
+
             foreach (var item in productImage)
             {
                 ProductImage p = new ProductImage();
                 p.ImageURL = item.FileName;
-                newProduct.ProductImages.Add(p);
+                item.SaveAs(Server.MapPath("/Uploads/Product/") + item.FileName);
+                newProduct.ProductImages.Add(p);               
             }
+
             if (ModelState.IsValid)
             {
                 db.Products.Add(newProduct);
