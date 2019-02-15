3dc7d1977f6252a1a757f5b6356349b3833ab0b7
anilkucukaydin <Section1@WISSEN>
2019-02-15 13:28:41 +0300

    Customer Password

--- E-Commerce/Controllers/LoginController.cs
+++ E-Commerce/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using E_Commerce.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,24 @@ namespace E_Commerce.Controllers
     public class LoginController : Controller
     {
         // GET: Login
+        CommerceContext db = new CommerceContext();
         public ActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Index(Customer newCustomer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(newCustomer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(newCustomer);
+        }
+       
+
     }
 }
